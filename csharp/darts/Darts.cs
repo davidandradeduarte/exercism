af80d33b373d9352851302349aee69df4e4aec96
David Duarte <davidandradeduarte@gmail.com>
2022-02-01 00:56:36 +0000

    csharp darts, roman-numerals

---
+++ csharp/darts/Darts.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public static class Darts
+{
+    public static int Score(double x, double y)
+    {
+        return Math.Sqrt(x * x + y * y) switch
+        {
+            var s when s <= 1 => 10,
+            var s when s <= 5 => 5,
+            var s when s <= 10 => 1,
+            _ => 0
+        };
+    }
+}
