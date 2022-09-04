6fba7280cb489682e8d40cb7c99a6a583a1d7c8c
David Duarte <davidandradeduarte@gmail.com>
2022-09-04 01:25:04 +0100

    rust rpg

---
+++ rust/role-playing-game/src/lib.rs
@@ -0,0 +1,35 @@
+pub struct Player {
+    pub health: u32,
+    pub mana: Option<u32>,
+    pub level: u32,
+}
+
+impl Player {
+    pub fn revive(&self) -> Option<Player> {
+        if self.health == 0 {
+            Some(Player {
+                health: 100,
+                mana: if self.level >= 10 { Some(100) } else { None },
+                level: self.level,
+            })
+        } else {
+            None
+        }
+    }
+
+    pub fn cast_spell(&mut self, mana_cost: u32) -> u32 {
+        if let Some(mana) = self.mana {
+            if mana >= mana_cost {
+                self.mana = Some(mana - mana_cost);
+                return mana_cost * 2;
+            }
+            return 0;
+        }
+        self.health = if mana_cost >= self.health {
+            0
+        } else {
+            self.health - mana_cost
+        };
+        0
+    }
+}
